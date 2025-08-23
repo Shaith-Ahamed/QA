5b6beb693f03e28d3e2bd32792db251202190fd6
Shaith-Ahamed <shaith1208@gmail.com>
2025-08-23 16:54:13 +0530

    Add CI/CD pipeline configuration with MySQL tests

---
+++ src/test/java/com/learn/demo/ui/BaseTest.java
@@ -0,0 +1,25 @@
+package com.learn.demo.ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
